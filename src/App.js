f77dbf0ad4c9e5ece3243f5d351bbb616da13938
Amit cesar <amitcesar27@gmail.com>
2021-07-25 23:57:54 -0300

    more aligns main content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Header } from "./components/Header.js";
 import { Footer } from "./components/Footer.js";
-import { PhonesList } from "./components/PhonesList.js";
+
 import { MainContent } from "./components/MainContent.js";
 function App() {
   return (
